4091b07f61477e11abdee2f8a9174e09be16c389
FlorianMu <f.muller493@laposte.net>
2019-05-15 15:32:05 +0200

    Modification on comment & tests

--- hpp/src/main/java/hpp/Comment.java
+++ hpp/src/main/java/hpp/Comment.java
@@ -1,23 +1,52 @@
 package hpp;
 
+import java.time.LocalDateTime;
+
 public class Comment implements Event {
-
+	private LocalDateTime timeStamp;
+	private String commentId;
+	private String userName;
+	private String commentReplied;
+	private int internScore;
+	
+	
+	
 	@Override
-	public void getId() {
+	public String getId() {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 	@Override
-	public void changeId() {
+	public void changeId(String newId) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void getTs() {
+	public LocalDateTime getTs() {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
+
+	@Override
+	public void decreaseInternScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewComment() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decreaseExternScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
